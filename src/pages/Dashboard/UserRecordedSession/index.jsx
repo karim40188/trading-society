14dcad54a53426145f3f24aa2220a9b555b22a8f
Karim <karim40189@gmail.com>
2025-05-06 23:00:10 +0300

    some edit

--- src/pages/Dashboard/UserRecordedSession/index.jsx
+++ src/pages/Dashboard/UserRecordedSession/index.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 import FooterProfile from "@/Components/Layouts/Dashboard/FooterProfile.jsx";
-import  from "@/Components/VideoController/index.jsx";
+import VideoController from "@/Components/VideoController/index.jsx";
 import LiveVideoCard from "./LiveVideoCard.jsx";
 
 import useApi from "@/api";
